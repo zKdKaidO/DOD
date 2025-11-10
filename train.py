42f7b9aa6d6946ecbda2fbc6bb89282b3cfc40b8
thuuytruuc <tranthuythuytruc@gmail.com>
2025-11-10 12:02:39 +0700

    Add config.yaml, modify train.py

--- train.py
+++ train.py
@@ -5,10 +5,8 @@ model = YOLO("yolov8n.pt")
 
 # Train the model on the COCO8 dataset for 100 epochs
 train_results = model.train(
+    cfg = "config.yaml",  # Path to model configuration file
     data="coco8.yaml",  # Path to dataset configuration file
-    epochs=100,  # Number of training epochs
-    imgsz=640,  # Image size for training
-    device="cpu",  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
 )
 
 # Evaluate the model's performance on the validation set
