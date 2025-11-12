e376e1764f48018fb88e0848f540b111de32618d
thuuytruuc <tranthuythuytruc@gmail.com>
2025-11-12 14:26:53 +0700

    trial 1: - mode: yolov8n-p2, pretrained: yolov8n, result folder: subset_01/train* - optimizer: AdamW - imgsize: 960 - lr0: 0.001, lrf: 0.2, weight_decay: 0.01

--- model.py
+++ model.py
@@ -1,21 +1,15 @@
 from ultralytics import YOLO
-
-# Load a pretrained YOLO11n model
-model = YOLO("yolov8n.pt")
+model = YOLO("ultralytics/cfg/models/v8/yolov8-p2.yaml")
 
 # Train the model on the COCO8 dataset for 100 epochs
-train_results = model.train(
-    cfg = "config.yaml",  # Path to model configuration file
-    data="coco8.yaml", # Path to dataset configuration file
-    pretrained = "yolov8n-p2.pt",  # Use pretrained weights
-)
+train_results = model.train(cfg = "config.yaml")
 
 # Evaluate the model's performance on the validation set
 metrics = model.val()
 
 # Perform object detection on an image
-results = model("../DOD/horse (1).jpg")  # Predict on an image
-results[0].show()  # Display results
+#results = model("../DOD/horse (1).jpg")  # Predict on an image
+#results[0].show()  # Display results
 
 # Export the model to ONNX format for deployment
 #path = model.export(format="onnx")  # Returns the path to the exported model
